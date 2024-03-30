1dcff0e3f127a8d46c3a236ba46d0e2894368b02
Austin Miller <vadim.sukhetskii@gmail.com>
2024-03-30 17:29:50 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -8,3 +8,15 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
 console.log(makeTransaction(10, 500));
+
+function findNumber(start, end, divisor) {
+  let number = 0;
+  for (let i = start; i <= end; i++) {
+    if (i % divisor === 0) {
+      number = i;
+      return number;
+    }
+  }
+}
+
+console.log(findNumber(2, 6, 5));
